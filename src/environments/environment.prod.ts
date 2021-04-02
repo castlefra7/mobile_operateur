52e7bed26482e1801a0306eeb659a475eef90d84
kaky <lachapelle.kaky@gmail.com>
2021-04-02 03:32:02 +0300

    change in capacitor.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: "http://localhost:8080"
+  url: "http://192.168.43.25:8080"
 
   // url: "http://10.0.2.2:8080"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: "http://localhost:8080"
+  url: "http://192.168.43.25:8080"
   // url: "https://operateur-kabs.herokuapp.com/"
   // url: "http://10.0.2.2:8080"
 };
